607470167d799dc6edab8a136b818008f5639cad
0z-cryptik <110707754+0z-cryptik@users.noreply.github.com>
2023-03-29 13:41:36 +0100

    few adjustments

--- omega.js
+++ omega.js
@@ -1,32 +1,31 @@
-var about = document.querySelectorAll('a')[0]
-var abmenu = document.getElementById('hov')
-var content = document.querySelectorAll('div')
+const content = document.querySelectorAll('div')
+const abmenu = $('div#hov')
+const about = $('a#ab')
 
-about.addEventListener('mouseover', function(){
-    if(abmenu.classList.contains('hidden')){
-        abmenu.classList.replace('hidden', 'block')
-    }else{
-        abmenu.classList.add('block')
-    }
+about.on('mouseover', function(){
+    abmenu.slideDown()
 })
 
-abmenu.addEventListener('mouseleave', function(){
-    this.classList.replace('block', 'hidden')
+abmenu.on('mouseleave', function(){
+    $(this).slideUp()
 })
 
+const hlinks = $('a#co')
+hlinks.on('mouseover', function(){
+    abmenu.slideUp()
+})
 
 window.onload =function(){
     content[0].remove()
     content[2].classList.remove('hidden')
 }
 
-var afrib = document.getElementById('afrib')
-var eurob = document.getElementById('eurob')
-var asiab = document.getElementById('asiab')
-
-var afrif = document.getElementById('afri')
-var eurof = document.getElementById('euro')
-var asiaf = document.getElementById('asia')
+const afrib = document.getElementById('afrib')
+const eurob = document.getElementById('eurob')
+const asiab = document.getElementById('asiab')
+const afrif = document.getElementById('afri')
+const eurof = document.getElementById('euro')
+const asiaf = document.getElementById('asia')
 
 
 
@@ -34,10 +33,14 @@ eurob.addEventListener('click', function(){
     if(afrib.classList.contains('bg-[#F28C28]') || asiab.classList.contains('bg-[#F28C28]')){
         afrib.classList.replace('bg-[#F28C28]', 'bg-white/0')
         asiab.classList.replace('bg-[#F28C28]', 'bg-white/0')
+        afrib.classList.remove('text-white')
+        asiab.classList.remove('text-white')
     }else{
         eurob.classList.replace('bg-white/0', 'bg-[#F28C28]')
+        eurob.classList.add('text-white')
     }
     eurob.classList.replace('bg-white/0', 'bg-[#F28C28]')
+    eurob.classList.add('text-white')
     if(afrif.classList.contains('block')){
         afrif.classList.replace('block', 'hidden')
     }else if(asiaf.classList.contains('block')){
@@ -53,10 +56,14 @@ asiab.addEventListener('click', function(){
     if(afrib.classList.contains('bg-[#F28C28]') || eurob.classList.contains('bg-[#F28C28]')){
         afrib.classList.replace('bg-[#F28C28]', 'bg-white/0')
         eurob.classList.replace('bg-[#F28C28]', 'bg-white/0')
+        eurob.classList.remove('text-white')
+        afrib.classList.remove('text-white')
     }else{
         asiab.classList.replace('bg-white/0', 'bg-[#F28C28]')
+        asiab.classList.add('text-white')
     }
     asiab.classList.replace('bg-white/0', 'bg-[#F28C28]')
+    asiab.classList.add('text-white')
     if(afrif.classList.contains('block')){
         afrif.classList.replace('block', 'hidden')
     }else if(eurof.classList.contains('block')){
@@ -72,10 +79,14 @@ afrib.addEventListener('click', function(){
     if(eurob.classList.contains('bg-[#F28C28]') || asiab.classList.contains('bg-[#F28C28]')){
         eurob.classList.replace('bg-[#F28C28]', 'bg-white/0')
         asiab.classList.replace('bg-[#F28C28]', 'bg-white/0')
+        asiab.classList.remove('text-white')
+        eurob.classList.remove('text-white')
     }else{
         afrib.classList.add('bg-[#F28C28]')
+        afrib.classList.add('text-white')
     }
     afrib.classList.replace('bg-white/0', 'bg-[#F28C28]')
+    afrib.classList.add('text-white')
     if(asiaf.classList.contains('block')){
         asiaf.classList.replace('block', 'hidden')
     }else if(eurof.classList.contains('block')){
@@ -95,18 +106,3 @@ for(var i=0; i < links.length; i++){
     })
 }
 
-const btns = document.querySelectorAll('button')
-
-for(var i=0; i < btns.length;i++){
-    btns[i].classList.add('hover:z-[5]')
-}
-
-const f3links= [links[1], links[2], links[3]]
-
-for(var i=0; i < f3links.length; i++){
-    f3links[i].addEventListener('mouseover', function(){
-        if(abmenu.classList.contains('block')){
-            abmenu.classList.replace('block', 'hidden')
-        }
-    })
-}
